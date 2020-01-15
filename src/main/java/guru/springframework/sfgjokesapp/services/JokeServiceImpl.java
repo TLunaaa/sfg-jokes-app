a22a3f3dee1a3b6b5adf736bb375ed556a8f20ac
Tomas <tomas_luna1996@hotmail.com>
2020-01-15 16:37:42 -0300

    service created. Closes #3

---
+++ src/main/java/guru/springframework/sfgjokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() { //spring will create it once, and then will be reused in that class
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
